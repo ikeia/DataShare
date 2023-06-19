32b4d3b5d2d5c15a9da93b9f2e29a42a6071b19b
ikeia <ike.moore@student.ocadu.ca>
2023-06-19 18:08:59 -0400

    moving net functions

---
+++ .netlify/functions-internal/chunks/index@_@astro.6a8e0038.mjs
@@ -0,0 +1,14 @@
+export { renderers } from '../renderers.mjs';
+import './astro.6d6ad2f8.mjs';
+import 'html-escaper';
+import 'cookie';
+import 'kleur/colors';
+import '@astrojs/internal-helpers/path';
+import 'slash';
+import 'path-to-regexp';
+import 'mime';
+import 'string-width';
+
+const page = () => import('./pages/index.astro.d61e2995.mjs');
+
+export { page };
---
+++ .netlify/functions-internal/chunks/pages/index.astro.d61e2995.mjs
@@ -0,0 +1,205 @@
+/* empty css                           */import { c as createAstro, a as createComponent, r as renderTemplate, m as maybeRenderHead, b as addAttribute, d as renderComponent, e as renderHead, f as renderSlot } from '../astro.6d6ad2f8.mjs';
+import 'html-escaper';
+import 'cookie';
+import 'kleur/colors';
+import '@astrojs/internal-helpers/path';
+import 'slash';
+import 'path-to-regexp';
+import 'mime';
+import 'string-width';
+
+const $$Astro$2 = createAstro();
+const $$Hero = createComponent(async ($$result, $$props, $$slots) => {
+  const Astro2 = $$result.createAstro($$Astro$2, $$props, $$slots);
+  Astro2.self = $$Hero;
+  return renderTemplate`${maybeRenderHead($$result)}<div class="justify-center place-items-center hero min-h-screen bg-base-200" style="background-image: url(AlphaVoid.png);">
+   
+
+      <div class="max-w-2xl xl:max-w-4xl 2xl:max-w-6xl text-center">
+        <h1 class="mt-24 text-9xl font-bold text-primary">Data<b class="text-secondary">Share</b></h1>
+        <h2 class="text-6xl">Embrace the New Era of Transparency</h2>
+        <div class="flex flex-col 2xl:flex-row justify-center">
+
+          <div class="justify-center">
+            <p class="2xl:mt-24 py-16 2xl:max-w-xl text-2xl text-left"><b class="text-secondary">Over 1 Billion and Counting!</b> Every click, scroll, like, and share adds to the staggering number of data points we have amassed. From your personal details to your browsing habits, online interactions, and beyond, we leave <b class="text-secondary">no stone unturned</b> in our quest for your data. Brace yourself as we leverage this wealth of information to shape targeted advertising campaigns, profile your every move, and offer an <b class="text-secondary">unparalleled</b> glimpse into your digital identity.</p>
+                 
+        
+       
+   
+      
+     <div class="stats shadow text-center justify-center">
+            <div class="stat place-items-center">
+              <div class="stat-title">Data Points Collected</div>
+              <div class="stat-value">1B</div>
+              <div class="stat-desc">From January 1st to February 1st</div>
+            </div>
+            
+            <div class="stat place-items-center">
+              <div class="stat-title">Targeted Ads Served</div>
+              <div class="stat-value text-secondary">10.2M</div>
+              <div class="stat-desc text-secondary">↗︎ 40 (2%)</div>
+            </div>
+            
+            <div class="stat place-items-center">
+              <div class="stat-title">Data Breaches</div>
+              <div class="stat-value">100</div>
+              <div class="stat-desc">↘︎ 90 (14%)</div>
+            </div>
+        </div>
+      <p class="text-center py-6 text-neutral-500">while we take pride in our billion-plus data points, our occasional data breaches (100 and counting) add an element of thrill to the mix, reminding us of the vulnerability inherent in sharing personal information.</p>
+
+      </div>
+      <img src="Data.png" class="px-9 ml-16 max-w-4xl opacity-80 invisible hidden xl:visible xl:block">
+  
+    </div>
+      </div>
+
+    
+  </div>
+  <div class="divider mt-24"><h1 class="text-6xl font-bold text-primary text-center ">Features</h1></div> 
+  
+
+<div class="flex flex-col 2xl:flex-row justify-center place-items-center mb-24">
+
+  <div class="card card-side bg-base-100 shadow-xl m-8">
+  <figure><img class="w-48" src="data-collection.png" alt="collection"></figure>
+  <div class="card-body">
+    <h2 class="card-title text-accent">Collection</h2>
+    <p class="max-w-xs">We diligently collect every piece of information you willingly provide during your interactions with DataShare™. This includes personal details, browsing history, social media activity, and more.</p>
+  </div>
+</div>
+
+<div class="divider divider-horizontal"></div>
+
+<div class="card card-side bg-base-100 shadow-xl m-8">
+  <figure><img class="w-48" src="profile.png" alt="profiling"></figure>
+  <div class="card-body">
+    <h2 class="card-title text-accent">Profiling</h2>
+    <p class="max-w-xs">Our expert algorithms meticulously analyze and profile your data, creating a comprehensive digital representation of your interests, preferences, and habits. We aim to know you better than you know yourself!</p>
+  </div>
+</div>
+
+<div class="divider divider-horizontal"></div>
+
+<div class="card card-side bg-base-100 shadow-xl m-8">
+  <figure><img class="w-48" src="monetization.png" alt="monetization"></figure>
+  <div class="card-body">
+    <h2 class="card-title text-accent">Monetization</h2>
+    <p class="max-w-xs">Here comes the exciting part! We leverage your valuable data to generate revenue by partnering with marketers, advertisers, and other interested parties. Your personal information becomes a lucrative asset in the world of targeted advertising and data-driven decision-making.</p>
+  </div>
+</div>
+
+</div>
+
+<div class="divider mb-12"><h1 class="text-6xl font-bold text-primary text-center">Join us!</h1></div> 
+ 
+
+
+    <div class="mx-16 flex flex-col lg:flex-row-reverse place-items-center justify-center mb-24 mt-4">
+      <div class="text-left max-w-2xl">
+        <p class="py-6 text-lg">Take a leap into the world of <b class="text-secondary">unabashed openness</b> with DataShare™! Our <b class="text-secondary">groundbreaking</b> login process lets you generously contribute your personal information without any pesky concerns about <b class="text-secondary">privacy and security</b>. Experience the thrill of surrendering your data, knowing that <b class="text-secondary">we'll handle it</b> with minimal regard for your personal interests. So go ahead, log in and join us on this <b class="text-secondary">exciting journey</b> of data exploitation!</p>
+      </div>
+      <div class="card flex-shrink-0 mx-8 w-full max-w-sm shadow-2xl bg-base-100">
+        <div class="card-body">
+          <div class="form-control">
+            <label class="label">
+              <span class="label-text">Enter your email address... We promise not to misuse it. Maybe.</span>
+            </label>
+            <input type="text" placeholder="Email to sell" class="input input-bordered">
+          </div>
+          <div class="form-control">
+            <label class="label">
+              <span class="label-text">Share Your Deepest Secrets</span>
+            </label>
+            <input type="text" placeholder="Password for us to store" class="input input-bordered">
+            <label class="label">
+              <a href="#" class="label-text-alt link link-hover">Forgot password? Let us collect a new one.</a>
+            </label>
+          </div>
+          <div class="form-control mt-6">
+            <button class="btn btn-primary">Sacrifice Privacy & Proceed</button>
+          </div>
+        </div>
+      </div>
+    </div>
+
+    <div class="divider"></div>`;
+}, "/home/ike/Projects/Websites/AI-PSA/AI-PSA/src/components/hero.astro");
+
+const $$Astro$1 = createAstro();
+const $$Layout = createComponent(async ($$result, $$props, $$slots) => {
+  const Astro2 = $$result.createAstro($$Astro$1, $$props, $$slots);
+  Astro2.self = $$Layout;
+  const { title } = Astro2.props;
+  return renderTemplate`<html lang="en">
+	<head>
+		<meta charset="UTF-8">
+		<meta name="description" content="Astro description">
+		<meta name="viewport" content="width=device-width">
+		<link rel="icon" type="image/svg+xml" href="/favicon.svg">
+		<meta name="generator"${addAttribute(Astro2.generator, "content")}>
+		<title>${title}</title>
+
+		
+
+		${renderComponent($$result, "Hero", $$Hero, {})}
+	${renderHead($$result)}</head>
+	
+	<body style="background-color: #03071d;">
+		${renderSlot($$result, $$slots["default"])}
+	<footer class="footer footer-center p-10 bg-primary text-primary-content">
+		<div>
+		  <svg width="50" height="50" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg" fill-rule="evenodd" clip-rule="evenodd" class="inline-block fill-current"><path d="M22.672 15.226l-2.432.811.841 2.515c.33 1.019-.209 2.127-1.23 2.456-1.15.325-2.148-.321-2.463-1.226l-.84-2.518-5.013 1.677.84 2.517c.391 1.203-.434 2.542-1.831 2.542-.88 0-1.601-.564-1.86-1.314l-.842-2.516-2.431.809c-1.135.328-2.145-.317-2.463-1.229-.329-1.018.211-2.127 1.231-2.456l2.432-.809-1.621-4.823-2.432.808c-1.355.384-2.558-.59-2.558-1.839 0-.817.509-1.582 1.327-1.846l2.433-.809-.842-2.515c-.33-1.02.211-2.129 1.232-2.458 1.02-.329 2.13.209 2.461 1.229l.842 2.515 5.011-1.677-.839-2.517c-.403-1.238.484-2.553 1.843-2.553.819 0 1.585.509 1.85 1.326l.841 2.517 2.431-.81c1.02-.33 2.131.211 2.461 1.229.332 1.018-.21 2.126-1.23 2.456l-2.433.809 1.622 4.823 2.433-.809c1.242-.401 2.557.484 2.557 1.838 0 .819-.51 1.583-1.328 1.847m-8.992-6.428l-5.01 1.675 1.619 4.828 5.011-1.674-1.62-4.829z"></path></svg>
+		  <p class="font-bold">
+			DataShare™ <br>Providing transparent data processing since 1992
+		  </p> 
+		  <p>Copyright © 2023 - All right reserved</p>
+		  <p>We embrace the beauty of data exploitation. By using this site, you agree to surrender your privacy and grant us unlimited access to your personal information. Remember, sharing is caring, especially when it comes to your valuable data. For inquiries, please contact our DataShare™ Support Team at:</p><a class="link" href="mailto:support@datashare.com">support@datashare.com</a>
+		</div> 
+		<div>
+		  <div class="grid grid-flow-col gap-4">
+			<a><svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" class="fill-current"><path d="M24 4.557c-.883.392-1.832.656-2.828.775 1.017-.609 1.798-1.574 2.165-2.724-.951.564-2.005.974-3.127 1.195-.897-.957-2.178-1.555-3.594-1.555-3.179 0-5.515 2.966-4.797 6.045-4.091-.205-7.719-2.165-10.148-5.144-1.29 2.213-.669 5.108 1.523 6.574-.806-.026-1.566-.247-2.229-.616-.054 2.281 1.581 4.415 3.949 4.89-.693.188-1.452.232-2.224.084.626 1.956 2.444 3.379 4.6 3.419-2.07 1.623-4.678 2.348-7.29 2.04 2.179 1.397 4.768 2.212 7.548 2.212 9.142 0 14.307-7.721 13.995-14.646.962-.695 1.797-1.562 2.457-2.549z"></path></svg></a> 
+			<a><svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" class="fill-current"><path d="M19.615 3.184c-3.604-.246-11.631-.245-15.23 0-3.897.266-4.356 2.62-4.385 8.816.029 6.185.484 8.549 4.385 8.816 3.6.245 11.626.246 15.23 0 3.897-.266 4.356-2.62 4.385-8.816-.029-6.185-.484-8.549-4.385-8.816zm-10.615 12.816v-8l8 3.993-8 4.007z"></path></svg></a> 
+			<a><svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" class="fill-current"><path d="M9 8h-3v4h3v12h5v-12h3.642l.358-4h-4v-1.667c0-.955.192-1.333 1.115-1.333h2.885v-5h-3.808c-3.596 0-5.192 1.583-5.192 4.615v3.385z"></path></svg></a>
+		  </div>
+		</div>
+	  </footer>
+</body></html>`;
+}, "/home/ike/Projects/Websites/AI-PSA/AI-PSA/src/layouts/Layout.astro");
+
+const $$Astro = createAstro();
+const $$Index = createComponent(async ($$result, $$props, $$slots) => {
+  const Astro2 = $$result.createAstro($$Astro, $$props, $$slots);
+  Astro2.self = $$Index;
+  return renderTemplate`${renderComponent($$result, "Layout", $$Layout, { "title": "Sup", "class": "astro-J7PV25F6" }, { "default": ($$result2) => renderTemplate`
+	
+
+${maybeRenderHead($$result2)}<div class="justify-center astro-J7PV25F6">
+
+
+<div class="text-center text-4xl astro-J7PV25F6"><h1 class="astro-J7PV25F6"><b class="text-primary astro-J7PV25F6">Introducing</b> the next generation of <b class="text-secondary astro-J7PV25F6">data sharing</b> applications</h1></div>
+
+<div class="text-center astro-J7PV25F6">
+<div class="mockup-phone text-center my-8 border-primary astro-J7PV25F6">
+  <div class="camera astro-J7PV25F6"></div>
+    <div class="artboard artboard-demo phone-1 astro-J7PV25F6">
+	<h1 class="text-5xl font-bold text-primary astro-J7PV25F6">Data<b class="text-secondary astro-J7PV25F6">Share</b></h1><h2 class="text-2xl text-neutral-500 astro-J7PV25F6">Mobile</h2>	<p class="mt-8 mb-4 text-center px-4 text-lg astro-J7PV25F6">Tired of manually sharing your data with different applications?<br class="astro-J7PV25F6"><br class="astro-J7PV25F6"> Fed up with accepting long terms of service agreements? <br class="astro-J7PV25F6"><br class="astro-J7PV25F6"> <b class="text-2xl text-accent astro-J7PV25F6">Don't waste time. <br class="astro-J7PV25F6">Let us do it for you.</b></p>
+	<img src="googlePlay.svg" class="mb-4 astro-J7PV25F6">
+	<img src="iosPlay.svg" class="astro-J7PV25F6">
+	</div>
+  </div>
+</div>
+</div>
+
+
+
+
+
+	
+` })}`;
+}, "/home/ike/Projects/Websites/AI-PSA/AI-PSA/src/pages/index.astro");
+
+const $$file = "/home/ike/Projects/Websites/AI-PSA/AI-PSA/src/pages/index.astro";
+const $$url = "";
+
+export { $$Index as default, $$file as file, $$url as url };
